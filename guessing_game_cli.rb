af89ec243f987b559432eb52e5a6e4d0fc31668d
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-26 04:25:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -28,7 +28,6 @@ def run_guessing_game
   
   puts "Guess a number between 1 and 6"
   user_input = gets.chomp
-  end
     
     if user_input == random_number
       puts "You guessed the correct number!"
