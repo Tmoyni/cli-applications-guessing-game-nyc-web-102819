325e029a6d197d940bf7e2e4c2269d055965c7c5
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-26 04:10:23 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -35,7 +35,7 @@ def run_guessing_game
     elsif user_input == exit_game
       puts "Goodbye!"
       
-    else 
+    else user_input 
       puts "Sorry! The computer guessed (random_number)."
     end 
   end 
